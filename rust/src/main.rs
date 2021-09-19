ac8de230c21d0b11ecbd7c88287fb8a399940867
Your Name <you@example.com>
2021-09-19 19:23:34 +0900

    rust: 0

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,7 +3,7 @@ use std::{cell::RefCell, collections::HashMap, env, error::Error, fs, rc::Rc};
 fn main() -> Result<(), Box<dyn Error>> {
     let code = fs::read_to_string(env::args().nth(1).ok_or("no file")?)?;
     let v = Parser::new(&code).list();
-    println!("#t");
+    evaluate_list(default_env(), &v)?;
     Ok(())
 }
 
@@ -26,7 +26,7 @@ impl Parser {
         }
     }
     fn list(&mut self) -> RefValue {
-        if self.tokens.is_empty() || self.tokens[0] == ")" {
+        if self.tokens.is_empty() || self.tokens.last().unwrap() == ")" {
             self.tokens.pop();
             return Value::None().rv();
         }
@@ -47,6 +47,7 @@ enum Value {
     Bool(bool),
     Pair(RefValue, RefValue),
     Symbol(String),
+    Func(fn(RefEnv, &RefValue) -> RefValue),
 }
 
 impl Value {
@@ -64,18 +65,94 @@ struct RefValue {
 
 struct Env {
     m: HashMap<String, RefValue>,
-    next: Rc<Env>,
+    next: Option<RefEnv>,
 }
 
-impl Env {
+#[derive(Clone)]
+struct RefEnv(Rc<RefCell<Env>>);
+
+impl RefEnv {
     fn lookup(&self, s: &str) -> RefValue {
-        if let Some(v) = self.m.get(s) {
+        if let Some(v) = self.0.borrow().m.get(s) {
             return v.clone();
         }
-        self.next.lookup(s)
+        self.0
+            .borrow()
+            .next
+            .as_ref()
+            .expect(&format!("not found: {}", s))
+            .lookup(s)
     }
-    fn ensure(&mut self, s: &str, v: RefValue) -> &Self {
-        self.m.insert(s.to_string(), v);
+    fn ensure(self, s: &str, v: RefValue) -> Self {
+        self.0.borrow_mut().m.insert(s.to_string(), v);
         return self;
     }
+}
+
+impl std::fmt::Display for RefValue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &*self.r.borrow() {
+            Value::None() => write!(f, "()"),
+            Value::Bool(true) => write!(f, "#t"),
+            Value::Bool(false) => write!(f, "#f"),
+            Value::Pair(x, y) => write!(f, "( {} {} )", x, y),
+            Value::Symbol(x) => write!(f, "{}", x),
+            Value::Func(_) => write!(f, "<func>"),
+        }
+    }
+}
+
+fn default_env() -> RefEnv {
+    RefEnv(Rc::new(RefCell::new(Env {
+        m: HashMap::new(),
+        next: None,
+    })))
+    .ensure(
+        "print",
+        Value::Func(|e, v| {
+            if let Value::Pair(ref x, _) = *(Rc::clone(&v.r)).borrow() {
+                let res = evaluate(e, x);
+                println!("{}", res);
+                Value::None().rv()
+            } else {
+                panic!("not pair")
+            }
+        })
+        .rv(),
+    )
+}
+
+fn evaluate(e: RefEnv, v: &RefValue) -> RefValue {
+    match &*v.r.borrow() {
+        Value::None() => Value::None().rv(),
+        Value::Bool(x) => Value::Bool(*x).rv(),
+        Value::Pair(ref x, ref y) => {
+            if let Value::Func(ref f) = &*Rc::clone(&evaluate(e.clone(), x).r).borrow_mut() {
+                return f(e, y);
+            }
+            panic!("not func")
+        }
+        Value::Symbol(x) => e.lookup(x),
+        _ => panic!("BUG"),
+    }
+}
+
+fn to_vec(v: &RefValue) -> Result<Vec<RefValue>, String> {
+    match &*v.r.borrow() {
+        Value::Pair(ref x, ref y) => {
+            let mut res = vec![x.clone()];
+            res.append(&mut to_vec(y)?);
+            Ok(res)
+        }
+        Value::None() => Ok(Vec::new()),
+        _ => Err(format!("unexpected type {}", v)),
+    }
+}
+
+fn evaluate_list(e: RefEnv, v: &RefValue) -> Result<RefValue, String> {
+    let mut res = Value::None().rv();
+    for x in to_vec(v)? {
+        res = evaluate(e.clone(), &x);
+    }
+    Ok(res)
 }
