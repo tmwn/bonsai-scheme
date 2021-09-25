a660eba0bdfbb2cbcd0cf061d43872b40c529361
Your Name <you@example.com>
2021-09-25 23:55:07 +0900

    rust2: 1

--- rust2/src/main.rs
+++ rust2/src/main.rs
@@ -1,22 +1,23 @@
 use std::{cell::RefCell, collections::HashMap, env, error::Error, fs, rc::Rc};
 use typed_arena::Arena;
-type Ctx<'a> = Arena<Val<'a>>;
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 fn main() -> Result<()> {
     let code = fs::read_to_string(env::args().nth(1).ok_or("no file")?)? + " $";
-    run(&Ctx::new(), code)?;
+    run(code)?;
     Ok(())
 }
 enum Val<'a> {
     Nil(),
     Bool(bool),
     Int(i64),
-    Pair(&'a Val<'a>, &'a Val<'a>),
+    Pair(V<'a>, V<'a>),
     Symbol(String),
-    Quote(&'a Val<'a>),
-    Func(fn(&'a Ctx<'a>, Vec<&Val>) -> Result<&'a Val<'a>>),
-    // Form(Box<dyn Fn(&Rc<Env>, Vec<&'a Val<'a>>) -> Result<&'a Val<'a>>>),
+    Quote(V<'a>),
+    Func(fn(Vec<V<'a>>) -> Result<Val<'a>>),
+    Form(fn(Ctx<'a>, &Rc<Env<'a>>, Vec<V<'a>>) -> Result<V<'a>>),
 }
+type V<'a> = &'a Val<'a>;
+type Ctx<'a> = &'a Arena<Val<'a>>;
 use Val::*;
 impl<'a> Val<'a> {
     fn to_vec(&self) -> Vec<&Val<'a>> {
@@ -43,17 +44,22 @@ impl<'a> std::fmt::Display for Val<'a> {
             Symbol(x) => write!(f, "{}", x),
             Quote(x) => write!(f, "'{}", x),
             Func(_) => write!(f, "<func>"),
+            Form(_) => write!(f, "<form>"),
         }
     }
 }
-fn run<'a>(ctx: &'a Ctx<'a>, mut code: String) -> Result<&'a Val<'a>> {
+fn run(mut code: String) -> Result<()> {
     let cs = "()'".chars();
     cs.for_each(|c| code = code.replace(c, &format!(" {} ", c)));
     let mut tokens = code.split_whitespace().rev().collect();
-    let root = list(ctx, &mut tokens)?;
-    eval_list(ctx, &default_env(ctx), root.to_vec())
+    let ctx = Arena::new();
+    {
+        let root = list(&ctx, &mut tokens)?;
+        eval_list(&ctx, &default_env(&ctx), root.to_vec())?;
+    }
+    Ok(())
 }
-fn list<'a>(ctx: &'a Ctx<'a>, tokens: &mut Vec<&str>) -> Result<&'a Val<'a>> {
+fn list<'a>(ctx: Ctx<'a>, tokens: &mut Vec<&str>) -> Result<&'a Val<'a>> {
     Ok(match tokens.pop().ok_or("empty")? {
         x if x == ")" || x == "$" => ctx.alloc(Nil()),
         x => {
@@ -62,7 +68,7 @@ fn list<'a>(ctx: &'a Ctx<'a>, tokens: &mut Vec<&str>) -> Result<&'a Val<'a>> {
         }
     })
 }
-fn value<'a>(ctx: &'a Ctx<'a>, tokens: &mut Vec<&str>) -> Result<&'a Val<'a>> {
+fn value<'a>(ctx: Ctx<'a>, tokens: &mut Vec<&str>) -> Result<&'a Val<'a>> {
     Ok(match tokens.pop().ok_or("empty")? {
         "(" => list(ctx, tokens)?,
         "#t" => ctx.alloc(Bool(true)),
@@ -74,18 +80,19 @@ fn value<'a>(ctx: &'a Ctx<'a>, tokens: &mut Vec<&str>) -> Result<&'a Val<'a>> {
         },
     })
 }
-fn eval_list<'a>(ctx: &'a Ctx<'a>, e: &Rc<Env<'a>>, v: Vec<&'a Val<'a>>) -> Result<&'a Val<'a>> {
+fn eval_list<'a>(ctx: Ctx<'a>, e: &Rc<Env<'a>>, v: Vec<&'a Val<'a>>) -> Result<&'a Val<'a>> {
     let res = v.into_iter().rev().map(|x| eval(ctx, e, x)).last();
     res.unwrap_or(Err("empty".into()))
 }
-fn eval<'a>(ctx: &'a Ctx<'a>, e: &Rc<Env<'a>>, v: &'a Val<'a>) -> Result<&'a Val<'a>> {
+fn eval<'a>(ctx: Ctx<'a>, e: &Rc<Env<'a>>, v: &'a Val<'a>) -> Result<&'a Val<'a>> {
     Ok(match v {
         Nil() | Bool(_) | Int(_) => v,
         Pair(x, y) => match eval(ctx, e, x)? {
             Func(f) => {
                 let u = y.to_vec().into_iter().map(|v| eval(ctx, e, v));
-                f(ctx, u.collect::<Result<_>>()?)?
+                ctx.alloc(f(u.collect::<Result<_>>()?)?)
             }
+            Form(f) => f(ctx, e, y.to_vec())?,
             x => return Err(format!("not func: {}", x).into()),
         },
         Symbol(x) => e.lookup(x)?,
@@ -115,22 +122,15 @@ impl<'a> Env<'a> {
         self.m.borrow_mut().insert(s.to_string(), v);
         self.clone()
     }
-    fn with_func(
-        self: Rc<Self>,
-        ctx: &'a Ctx<'a>,
-        s: &str,
-        f: fn(&'a Ctx<'a>, Vec<&Val<'_>>) -> Result<&'a Val<'a>>,
-    ) -> Rc<Self> {
-        self.ensure(s, ctx.alloc(Val::<'a>::Func(f)))
+    fn with_func(self: Rc<Self>, ctx: Ctx<'a>, s: &str, f: fn(Vec<V>) -> Result<Val>) -> Rc<Self> {
+        self.ensure(s, ctx.alloc(Func(f)))
     }
 }
-fn default_env<'a>(ctx: &'a Ctx<'a>) -> Rc<Env<'a>> {
+fn default_env<'a>(ctx: Ctx<'a>) -> Rc<Env<'a>> {
     Env::new(None)
-        .with_func(ctx, "-", |ctx, v| {
-            Ok(ctx.alloc(Int(v[1].int()? - v[0].int()?)))
-        })
-        .with_func(ctx, "print", |ctx, v| {
+        .with_func(ctx, "print", |v| {
             print!("{}", v[0]);
-            Ok(ctx.alloc(Nil()))
+            Ok(Nil())
         })
+        .ensure("begin", ctx.alloc(Form(|ctx, e, v| eval_list(ctx, e, v))))
 }
